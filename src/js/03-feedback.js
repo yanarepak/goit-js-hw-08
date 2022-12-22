451b6d3aa21d2ad16c1ba99b71a0a206189cde29
yanarepak <112949438+yanarepak@users.noreply.github.com>
2022-12-22 20:39:07 +0200

    finish 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,36 @@
 import throttle from 'lodash.throttle';
 
 const formEl = document.querySelector('.feedback-form');
-console.log(formEl);
+const refs = {
+    email: document.querySelector('input'),
+    message: document.querySelector('textarea')
+}
 
-const STORAGE_KEY = "feedback-form-state";
+const FEEDBACK_FORM = "feedback-form-state"
+
+formEl.addEventListener('input', throttle(handleFormInput, 500)); 
+formEl.addEventListener('submit', handlerFormSubmit);
+
+const formData = {};
+
+function handleFormInput(event){
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(FEEDBACK_FORM, JSON.stringify(formData));
+};
+
+function handlerFormSubmit(event){
+    console.log(JSON.parce(localStorage.getItem(FEEDBACK_FORM)));
+    event.preventDefault();
+    formEl.reset();
+    localStorage.removeItem(FEEDBACK_FORM);
+}
+
+function populateTextarea(){
+    if(JSON.parse(localStorage.getItem(FEEDBACK_FORM)) === null){
+        return
+    }
+    refs.message.value = JSON.parse(localStorage.getItem(FEEDBACK_FORM)).message || '' ;
+    refs.email.value = JSON.parse(localStorage.getItem(FEEDBACK_FORM)).email || '';
+}
+populateTextarea();
+ 
